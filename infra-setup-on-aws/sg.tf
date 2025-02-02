68ac12f3e43c352f44af07fbec94193533e7fd6b
khwahishindoria <khwahishindoria@gmail.com>
2025-02-02 16:56:39 +0530

    resource config added

--- infra-setup-on-aws/sg.tf
+++ infra-setup-on-aws/sg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "prod-vpc-SG" {
         from_port = ingress.value
         to_port = ingress.value
         protocol = "tcp"
-        cidr_blocks = [ "10.0.0.0/16" ]
+        cidr_blocks = [ "0.0.0.0/0" ]
       }
     }
 
